1c960575fe9b5bfb67d383056958df8aac898f0d
Joshua Johnston <36349396+JJohnston89@users.noreply.github.com>
2018-02-27 00:45:16 -0500

    Create display_menu.c

---
+++ display_menu.c
@@ -0,0 +1,15 @@
+/*	This function displays a menu for the client 
+	Name: Josh Johnston
+	Date: 11/18/2015
+*/
+#include "protot.h"
+
+void  display_menu()
+{
+	printf("1: Host current time\n");
+	printf("2: Host uptime\n");
+	printf("3: Host available memory\n");
+	printf("4: Host number of current users\n");
+	printf("5: Quit\n");
+	
+}
