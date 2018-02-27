65fb7ab0b5b8860160f9c3c8bdebf451f95d9689
Joshua Johnston <36349396+JJohnston89@users.noreply.github.com>
2018-02-27 00:52:56 -0500

    Create protot.h

---
+++ protot.h
@@ -0,0 +1,15 @@
+/* This header file consist of the prototypes of display_menu(), get_user_input(), and main().
+   display_menu() is defined in display_menu.c
+   get_user_input() is defined in get_user_input.c
+   main is defined in main.c
+   Name: Joshua Johnston
+   Date: 11/18/2015
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void display_menu();
+int get_user_input();
+int main ( int argc, char *argv[]);
+
